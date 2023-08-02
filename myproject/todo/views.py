d56fe11fbc5a126772ccece279cf6c87ff051324
Zain <learnerstudent001@gmail.com>
2023-08-02 10:48:10 +0500

    Update, Delete Functionality Added

--- myproject/todo/views.py
+++ myproject/todo/views.py
@@ -15,3 +15,26 @@ def index(request):
 
     context = {'tasks': tasks, 'form': form}
     return render(request, 'list.html', context)
+
+def updateTask(request, pk):
+    task = Task.objects.get(id=pk)
+    form = TaskForm(instance=task)
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+    context = {'form': form}
+    return render(request, 'update_task.html', context)
+
+def deleteTask(request, pk):
+    task = Task.objects.get(id=pk)
+
+    if request.method == 'POST':
+        task.delete()
+        return redirect('/')
+
+    context = {'task': task}
+    return render(request, 'delete_task.html', context)
